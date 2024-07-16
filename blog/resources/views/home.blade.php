056f42478049bde0ea0d55ae2fbcbefffab0f0f4
unknown <miguel.a.torralba@gmail.com>
2024-07-16 08:18:14 +0200

    plantillas

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
+@extends('layout.app')
+@section('title', 'Home')
+@push('css')
+    <style>
+        body{
+            background-color: #f5f5f5;
+        }
+    </style>
+@endpush
+@section('content')
     <div class="max-w-4xl mx-auto px-4">
         <!--<h1>Bienvenido a la pagina principal</h1>-->
         <x-alert2 type="info" class="mb-4">
@@ -18,5 +18,5 @@
         </x-alert2>
         <p>Hola mundo</p>
     </div>
-</body>
-</html>
+@endsection
+    
