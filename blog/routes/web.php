69b02baacbc86594fffd2d83cabd8ff4bda45c09
unknown <miguel.a.torralba@gmail.com>
2024-07-19 07:27:29 +0200

    elocuetn

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 //get => desde la url
 //post => desde un formulario con info oculta
@@ -14,6 +15,39 @@ Route::get('/', HomeController::class); //esta forma de invocarlo es por el mét
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::get('prueba', function(){
+    /*
+    //CREAR REGISTROS
+    $post = new Post;
+    $post->title = 'Titulo de prueba 3';
+    $post->content = 'Este es el contenido 3';
+    $post->categoria = 'Categoria de prueba 3';
+    $post->save();
+    */
+
+    //$post = Post::find(1);
+    /*
+    ///Modificar un registro
+    $post = Post::where('title', 'Titulo de prueba 1')->first();
+    $post->categoria='Desarrollo Web';
+    $post->save();
+    return $post;
+    */
+    /*
+    //Obtener registros
+    $post = Post::orderBy('id', 'DESC')
+        ->select('id', 'title')
+        ->take(5)
+        ->get();
+    return $post;
+    */
+
+    //eliminar un registro
+    $post = Post::find(1);
+    $post->delete();
+    return "Eliminado";
+    /*return $post;*/
+});
 
 
 /*Route::get('/posts/{post}/{categoria?}', function($post, $categoria = null){
@@ -24,4 +58,3 @@ Route::get('/posts/{post}', [PostController::class, 'show']);
     }
     
 });*/
-
